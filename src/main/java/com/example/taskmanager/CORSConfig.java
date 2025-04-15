ec10f0d2afcff02a3da0a0d6ac5397ec70dde5fc
Ashraf <ashrafhashashx@gmail.com>
2025-04-16 00:22:49 +0200

    readjust for both local testing and deployment

--- src/main/java/com/example/taskmanager/CORSConfig.java
+++ src/main/java/com/example/taskmanager/CORSConfig.java
@@ -1,5 +1,6 @@
 package com.example.taskmanager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -10,18 +11,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CORSConfig {
 
+    // This gets its value from application.properties file:
+    @Value("${my.cors.config.allowing.react.app}") String myCorsConfigForTheReactApp;
+
     @Bean
     public CorsFilter corsFilter() {
         // Create the CORS configuration
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowCredentials(true);
-        // The following line is basically who is allowed to use this backend. When I deploy it on Render, for example,
-        // I will just set an environment variable when filling out the form on the website. instead of just hard-coding at
-        // something like this:
-        // corsConfig.addAllowedOrigin("https://my-react-app-py6s.onrender.com"); // React app's URL on Render for example
-        // So, this is just for the local testing:
-        corsConfig.addAllowedOrigin("http://localhost:3000"); // React app on my computer
-        corsConfig.addAllowedOrigin("${CORSCONFIG_ALLOWING_REACT_APP}"); // React app on my computer
+        corsConfig.addAllowedOrigin("https://example-webapp-spring-react-postgres-7za6.onrender.com");
+        corsConfig.addAllowedOrigin(myCorsConfigForTheReactApp); // React app on my computer
         corsConfig.addAllowedHeader("*");
         corsConfig.addAllowedMethod("*");
 
